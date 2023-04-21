a059331213947b91b7dcab50077559438caa3042
anikak28 <122763891+anikak28@users.noreply.github.com>
2023-04-21 17:36:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import math 
+import matplotlib.pyplot as plt
 
 # df = pd.read_csv('Retail.OrderHistory.1.csv')
 # print(df.head())
@@ -36,8 +37,27 @@ df.columns = df.columns.str.strip()
 
 
 
-print(df["Shipment Item Subtotal Tax"].sum())
-print(df["Shipment Item Subtotal Tax"].sum() / df["Total Owed"].sum())
+# print(df["Shipment Item Subtotal Tax"].sum())
+# df["Shipment Item Subtotal Tax"] = pd.to_numeric(df["Shipment Item Subtotal Tax"], errors='coerce')
+# df["Total Owed"] = pd.to_numeric(df["Total Owed"], errors='coerce')
+# df = df.dropna(subset=["Shipment Item Subtotal Tax", "Total Owed"])
+
+# print(df["Shipment Item Subtotal Tax"].sum() / df["Total Owed"].sum())
 
 df['Order Date'] = pd.to_datetime(df['Order Date'])
 print(df.head())
+
+print(df.plot.bar(x='Order Date', y='Total Owed', rot=90))
+plt.show()
+
+print(df.plot.bar(x='Order Date', y='Total Owed', rot=90, figsize=(20,10)))
+plt.show()
+
+daily_orders = df.groupby('Order Date').sum()["Total Owed"]
+daily_orders.head()
+
+print(daily_orders.plot.bar(figsize=(20,10)))
+plt.show()
+
+print(daily_orders.plot.bar(figsize=(20, 10), color='#61D199'))
+plt.show
